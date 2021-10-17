ab90753a4d08be8c54918abbc87724527d2b596d
PhiGei2000 <Philipp-Geil@outlook.com>
2021-10-17 23:43:27 +0200

    Start to implement terms

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "math/expressions.hpp"
+#include "math/expressions/simplifier.hpp"
 
 using namespace cas::math;
 
@@ -22,17 +23,18 @@ int main(int argCnt, char** args) {
     Variable v1 = 'x';
     Variable v2 = 'y';
 
-    std::cout << "c1: " << &c1 << std::endl;
-    std::cout << "c2: " << &c2 << std::endl;
-    std::cout << "v1: " << &v1 << std::endl;
+    auto s1 = (c1 * v1)->add(&v2);
+    auto s2 = (v2 * c1)->add(&v1);
 
-    auto s1 = (c1 + v1)->add(&v2);
-    auto s2 = (v2 + v1)->add(&c1);
+    Term* t1 = Simplifier::convertToTerm(s1);
+    std::cout << t1->toString() << std::endl;
 
-    match(s1, s2);
+    Term* t2 = Simplifier::convertToTerm(s2);
+    std::cout << t2->toString() << std::endl;
+
+    std::cout << "t1 == t2 ? " << t1->equals(t2) << std::endl;
 
     delete s1;
-    delete s2;
 
     return 0;
 }
